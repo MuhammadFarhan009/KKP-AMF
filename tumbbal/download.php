5848b2f87a80172a1d5f57da5086182fb1072a45
MuhammadFarhan009 <farhan_m1@mhs.unsyiah.ac.id>
2024-07-23 09:43:46 +0700

    add php file

---
+++ tumbbal/download.php
@@ -0,0 +1,122 @@
+<?php
+
+if (isset($_GET['variable'])) {
+    $variable = $_GET['variable'];
+    $api_url = "https://webapi.bps.go.id/v1/api/list/model/data/lang/ind/domain/1100/var/$variable/key/e437040ac2bc6886742a0bfab5a46355/";
+    // $url = "https://webapi.bps.go.id/v1/api/list/model/data/lang/ind/domain/1100/var/$variable/key/e437040ac2bc6886742a0bfab5a46355/";
+    // echo $variable; // Outputs: Hello, World!
+}
+
+
+$response = file_get_contents($api_url);
+$data = json_decode($response, true);
+
+$idvariabel = $data["var"][0]["val"];
+$jumlahbaris = count($data["vervar"]);
+$jumlahkarakteristik = count($data["turvar"]);
+$jumlahtahun = count($data["tahun"]);
+$jumlahturtahun = count($data["turtahun"]);
+
+if( isset($_GET['title'])){
+    $title = $_GET['title'];
+    $filename = $title . ".csv";
+    header('Content-Type: text/csv');
+    
+    header('Content-Disposition: attachment;filename="' . $filename . '"');
+}
+    
+
+$output = fopen('php://output', 'w');
+if (isset($_GET['variable'])) {
+    $variable = $_GET['variable'];
+    $url = "https://webapi.bps.go.id/v1/api/list/model/data/lang/ind/domain/1100/var/$variable/key/e437040ac2bc6886742a0bfab5a46355/";
+    // echo $variable; // Outputs: Hello, World!
+}
+
+// Generate the header rows
+if ($jumlahturtahun == 1 && $jumlahkarakteristik == 1) {
+    fputcsv($output, [$data["labelvervar"]]);
+    fputcsv($output, [$data["var"][0]["label"]]);
+    $header = [];
+    foreach ($data["tahun"] as $tahun) {
+        $header[] = $tahun["label"];
+    }
+    fputcsv($output, $header);
+} else if ($jumlahturtahun > 1 && $jumlahkarakteristik == 1) {
+    fputcsv($output, [$data["labelvervar"]]);
+    fputcsv($output, [$data["var"][0]["label"]]);
+    $header1 = [];
+    foreach ($data["tahun"] as $tahun) {
+        $header1[] = $tahun["label"];
+    }
+    fputcsv($output, $header1);
+    $header2 = [];
+    foreach ($data["tahun"] as $tahun) {
+        foreach ($data["turtahun"] as $turtahun) {
+            $header2[] = $turtahun["label"];
+        }
+    }
+    fputcsv($output, $header2);
+} else if ($jumlahturtahun == 1 && $jumlahkarakteristik > 1) {
+    fputcsv($output, [$data["labelvervar"]]);
+    fputcsv($output, [$data["var"][0]["label"]]);
+    $header1 = [];
+    foreach ($data["turvar"] as $turvar) {
+        $header1[] = $turvar["label"];
+    }
+    fputcsv($output, $header1);
+    $header2 = [];
+    foreach ($data["turvar"] as $turvar) {
+        foreach ($data["tahun"] as $tahun) {
+            $header2[] = $tahun["label"];
+        }
+    }
+    fputcsv($output, $header2);
+} else if ($jumlahturtahun > 1 && $jumlahkarakteristik > 1) {
+    fputcsv($output, [$data["labelvervar"]]);
+    fputcsv($output, [$data["var"][0]["label"]]);
+    $header1 = [];
+    foreach ($data["turvar"] as $turvar) {
+        $header1[] = $turvar["label"];
+    }
+    fputcsv($output, $header1);
+    $header2 = [];
+    foreach ($data["turvar"] as $turvar) {
+        foreach ($data["tahun"] as $tahun) {
+            $header2[] = $tahun["label"];
+        }
+    }
+    fputcsv($output, $header2);
+    $header3 = [];
+    foreach ($data["turvar"] as $turvar) {
+        foreach ($data["tahun"] as $tahun) {
+            foreach ($data["turtahun"] as $turtahun) {
+                $header3[] = $turtahun["label"];
+            }
+        }
+    }
+    fputcsv($output, $header3);
+}
+
+// Generate the data rows
+for ($i = 0; $i < $jumlahbaris; $i++) {
+    $row = [$data["vervar"][$i]["label"]];
+    for ($j = 0; $j < $jumlahkarakteristik; $j++) {
+        for ($k = 0; $k < $jumlahtahun; $k++) {
+            for ($l = 0; $l < $jumlahturtahun; $l++) {
+                $id_data = $data["vervar"][$i]["val"] .
+                           $idvariabel .
+                           $data["turvar"][$j]["val"] .
+                           $data["tahun"][$k]["val"] .
+                           $data["turtahun"][$l]["val"];
+                $data_value = isset($data["datacontent"][$id_data]) ? $data["datacontent"][$id_data] : "-";
+                $row[] = $data_value;
+            }
+        }
+    }
+    fputcsv($output, $row);
+}
+
+fclose($output);
+exit();
+?>
---
+++ tumbbal/index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>List Halaman semua data</title>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        th, td {
+            border: 1px solid black;
+            padding: 10px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+
+        .table-title {
+            font-weight: bold;
+            font-size: 18px;
+        }
+
+        .table-subname {
+            font-weight: bold;
+            font-size: 18px;
+            text-align: right;
+        }
+
+        .table-link {
+            color: blue;
+            text-decoration: underline;
+        }
+    </style>
+</head>
+<body>
+    <?php
+
+    $pages = isset($_GET["page"]) ? $_GET["page"] : 1;
+
+    $url = "https://webapi.bps.go.id/v1/api/list/model/var/lang/ind/domain/1100/key/e437040ac2bc6886742a0bfab5a46355/page/$pages";
+
+
+    $curl = curl_init(); // Initialize cURL
+    curl_setopt($curl, CURLOPT_URL, $url); // Set URL
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $response = curl_exec($curl); // Execute cURL
+    curl_close($curl); // Close cURL
+
+    $response = json_decode($response, TRUE);
+
+    // echo count($response["data"][1])
+
+    $page = $response["data"][0]["page"];
+    $totalPage = $response["data"][0]["pages"];
+
+    // echo "$page $totalPage";
+    
+    $count = count($response["data"][1]);
+    $array = [];
+    $subName = [];
+    $varId = [];
+    for ($i=0; $i < $count ; $i++) { 
+        $title = $response["data"][1][$i]["title"];
+        $sub = $response["data"][1][$i]["sub_name"];
+        $varIds = $response["data"][1][$i]["var_id"];
+        array_push($array, $title );
+        array_push($subName, $sub);
+        array_push($varId, $varIds);
+        // array_push());
+    }
+
+
+    ?>
+
+
+<table border="1">
+    <thead>
+        <tr>
+            <th class="table-title">Judul Tabel</th>
+            <th class="table-subname">Sub Name</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($array as $index => $tumbalItem): ?>
+            <tr>
+                <td class="table-title"> 
+                    <a href="test.php?variable=<?php echo urlencode($varId[$index]); ?>" class="table-link"><?php echo $tumbalItem; ?></a>
+                </td>
+                <td class="table-subname">
+                    <?php if (isset($subName[$index])): ?>
+                        <?php echo $subName[$index]; ?>
+                    <?php endif; ?>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+
+<?php
+$totalPage = $response["data"][0]["pages"];
+for ($i=1; $i < $totalPage ; $i++) { 
+    echo "<a href='?page=".$i."'>[".$i."]</a>";
+}
+?>
+
+
+    
+</body>
+</html>
